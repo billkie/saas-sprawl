bc0fac822f209857a079bc198b365c9765e1039b
Bill Kiernan <trillbo@gmail.com>
2025-02-25 12:17:19 +0000

    refactor: Modify Auth0 route handler to support dynamic route parameters

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,3 +1,6 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth(); 
+export const GET = async (req: Request, { params }: { params: { auth0: string[] } }) => {
+  const auth0Params = await Promise.resolve(params.auth0);
+  return handleAuth()(req);
+}; 
